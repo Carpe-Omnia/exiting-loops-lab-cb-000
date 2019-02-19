8c4a097cde1444be88f01a8a84ccc94d67629c57
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 21:21:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function keepGoing(array, changeValue, skipValue) {
 }
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
-    if (findFn(array[i]) {return array[i]}
+    if (findFn(array[i])) {return array[i]}
   }
 }
