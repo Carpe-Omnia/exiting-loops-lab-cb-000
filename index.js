69fd3f451415be97a659893a19828a3ed30bc83f
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 21:14:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function breakOut(array, changeValue, stopValue) {
     if (element !== stopValue) {element = changeValue}
     else {return array}
   })
-
+  return array
 }
 function keepGoing(array, changeValue, skipValue) {}
 function findBy(array, findFn) {}
